d63f9cebf79f852b4e0c89985eb31b9935a7bfa6
hanna dereje <hannadereje11@gmail.com>
2021-07-25 20:40:16 +0300

    models

---
+++ yenetabackend1/Models/Messages.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+
+    content: {
+         type: String, 
+         required: true 
+        },
+    date: {
+         type: Date, 
+         required: true 
+        },
+    user: {
+         type: Schema.Types.ObjectId,
+         ref:"UserClass" ,
+         required:true 
+       }
+}, {
+        timestamps: true
+    })
+
+class Message {
+
+    constructor(content, date, user) {
+        this.content = content;
+        this.date = date;
+        this.user = user;
+    }
+
+}
+
+
+MessageSchema.loadClass(Message)
+
+module.exports = mongoose.model('MessageClass', MessageSchema)

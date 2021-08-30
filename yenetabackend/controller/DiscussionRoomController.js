36d8c904f8dba6b6b66f7f223c3d975acd2f7ae0
HiwotTadesse <hiwottadese78@gmail.com>
2021-08-30 07:54:52 -0700

    quiz component and controller

--- yenetabackend/controller/DiscussionRoomController.js
+++ yenetabackend/controller/DiscussionRoomController.js
@@ -1,43 +1,111 @@
 var options = {
-    cors: true,
-    origins: ["http://localhost:3000"]
+    cors:true, 
+    origins:["http://localhost:3000"]
 }
 
 const formatMessage = require("./FormatMessage")
 const botName = "Yeneta bot"
+const users =[]
+
 
 const socketio = require("socket.io")
-class DiscussionRoomController {
+class DiscussionRoomController{
 
-    constructor(server) {
+    constructor(server){
 
+        var io = socketio(server,options)
+        
+        io.on("connection", socket=>{
 
-        var io = socketio(server, options)
-        io.on("connection", socket => {
-
-            this.welcome(socket)
-            this.disconnect(socket, io)
-            this.chatMessage(socket, io)
-        })
-    }
-
-    welcome(socket) {
-        socket.broadcast.emit("message", formatMessage(botName, "A user has joined the chat"))
-    }
-
-    disconnect(socket, io) {
-        socket.on("disconnect", () => {
-            io.emit("message", formatMessage(botName, "A user has left the chat"))
-        })
-    }
-
-    chatMessage(socket, io) {
-        socket.on("chatMessage", msg => {
-            console.log(msg)
-            io.emit("chatMessage", formatMessage("USER", msg))
+            this.socketMessages(socket, io)
+        
         })
 
+        this.userLeave = this.userLeave.bind(this)
+        this.userjoin = this.userjoin.bind(this)
+        this.getCurrentUser = this.getCurrentUser.bind(this)
+        this.getTopicUsers = this.getTopicUsers.bind(this)
     }
+
+    socketMessages (socket, io){
+
+        socket.on("joinroom", ({username, topic})=>{
+            const user = this.userjoin(socket.id, username, topic)
+            socket.join(user.topic)
+
+            console.log(user)
+            socket.emit("message", formatMessage(botName, "Welcome to Yeneta Discussion Room"))
+            socket.broadcast.to(user.topic).emit("message", formatMessage(botName, `A ${user.username} has joined the chat`))
+
+            io.to(user.topic).emit("TopicUsers",{
+                topic:user.topic,
+                users:this.getTopicUsers()
+            })
+            
+        })
+
+         
+      
+        socket.on("disconnect", ()=>{
+
+            const user = this.userLeave(socket.id)
+            if(user){
+                io.to(user.topic).emit("message", formatMessage(botName,`A ${user.username} has left the chat`))
+            
+                io.to(user.topic).emit("TopicUsers",{
+                    topic:user.topic,
+                    users:this.getTopicUsers()
+                })
+            }
+
+        })
+    
+        socket.on("chatMessage", msg=>{
+
+            const user = this.getCurrentUser(socket.id)
+            console.log(user)
+            io.emit("chatMessage", formatMessage(user.username, msg))
+        })
+
+        socket.on("likedMessage", ({time, like, message})=>{
+            socket.broadcast.emit("likedMessage", 
+            {time,like, message})
+
+        })
+
+    }
+
+    userjoin(socket_id, username, topic){
+
+        const user = {socket_id, username, topic}
+
+        users.push(user)
+        return users;
+       
+      
+    }
+
+    
+
+    userLeave(id){
+
+        const index = users.findIndex(user => user.socket_id === id)
+        if(index !== -1){
+            return users.splice(index, 1)[0]
+        }
+    }
+
+    getCurrentUser(id){
+        console.log(users)
+        return users.find(user => user.socket_id===id)
+    }
+
+    getTopicUsers(){
+        return users
+    }
+
+    
+    
 }
 
 
--- yenetabackend/controller/QuizResultController.js
+++ yenetabackend/controller/QuizResultController.js
@@ -1,38 +1,82 @@
-class QuizResultController {
+class QuizResultController{
 
-    constructor(questionService, quizAnswerService, quizService, quizResultService) {
+    constructor(quizService, quizAnswerService, questionService, quizResultService){
         this.questionService = questionService;
-        this.quizResultService = quizResultService;
+        this.quizResultService = quizResultService
         this.quizService = quizService
         this.quizAnswerService = quizAnswerService
-        this.checkResultForActivity = this.checkResultForQuiz.bind(this)
-
+        this.checkResultForQuiz = this.checkResultForQuiz.bind(this)
+        this.checkEntranceQuiz = this.checkEntranceQuiz.bind(this)
+        
     }
-    checkResultForQuiz(req, res) {
-        this.quizService.getOne(req.params.id)
-        then((quiz) => {
-            this.quizAnswerService.getByQuiz(quiz._id)
-                .then((studentAnswers) => {
-                    for (question in quiz.questions) {
-                        this.questionService.getOne(question.id)
-                        then((result) => {
-                            const answer = result.answer
 
-                            for (let i = 0; i < studentAnswers.length; i++) {
-                                if (studentAnswers[i] == answer) {
-                                    total = total + 1;
-                                }
+    checkEntranceQuiz(req, res){
 
+        console.log(req.body)
+
+        const correctAnswers = {}
+        const incorrectAnswers={}
+        const ids=[]
+
+        const answer = Object.entries(req.body.answer);
+
+        for (let i = 1; i < answer.length; i++) {
+            ids.push(answer[i][0].split("_")[1])     
+        }
+        return this.questionService.getMany(ids)
+                .then((question)=>{
+
+                    var result = 0;
+                    for (let i = 0; i < question.length; i++) {
+
+                        for (let j = 1; j < answer.length; j++) {
+
+                            if(question[i]._id == answer[j][0].split("_")[1] && question[i].answer == answer[j][1]){
+                                        
+                                result++;
+                                correctAnswers[j] = answer[j][0].split("_")[1]
+                               
                             }
-                        })
+                        }
+                        
                     }
 
-                    res.status(200).json(total);
-                })
+                    res.status(200).json({"result":result, "correct":correctAnswers})
 
-        })
+                }).catch((err)=>{
+                    res.status(403)
+                    console.log("err");
+                }) 
 
+         
 
+    }
+
+    checkResultForQuiz(req, res) {
+        this.quizService.getOne(req.params.id)
+            then((quiz)=>{
+                this.quizAnswerService.getByQuiz(quiz._id)
+                    .then((studentAnswers)=>{
+                        for (question in quiz.questions) {
+                            this.questionService.getOne(question.id)
+                                then((result)=>{
+                                    const answer = result.answer
+
+                                    for (let i = 0; i < studentAnswers.length; i++) {
+                                        if (studentAnswers[i] == answer) {
+                                            total = total + 1;
+                                        }
+                        
+                                    }
+                                })
+                        }
+
+                        res.status(200).json(total);
+                    })
+
+            })
+        
+       
 
 
     }
